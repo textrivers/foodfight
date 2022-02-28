bae9986aa6468c626ce24d4c459e9adbded6e756
textrivers <jon.p.woodward@gmail.com>
2022-02-28 17:28:12 -0500

    3D start

---
+++ Scenes/Level3D.gd
@@ -0,0 +1,21 @@
+extends Spatial
+
+export var board_size: Vector2 = Vector2(5, 5)
+export var tile_size: float = 1
+var tile = preload("res://Scenes/FloorTile3D.tscn")
+var offset: Vector2
+
+func _ready():
+#	"res://Scenes/FloorTile3D.tscn"
+	build()
+
+func build():
+	for x in board_size.x:
+		for y in board_size.y: 
+			var new_tile = tile.instance()
+			new_tile.translation.x = (x * tile_size) - (board_size.x / 2)
+			new_tile.translation.z = (y * tile_size) - (board_size.y / 2)
+			if (x + y) % 2 == 0:
+				new_tile.set_material_override(null)
+			add_child(new_tile)
+			
