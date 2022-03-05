2352cede7f17283050433d124faa66d6f5616ee9
textrivers <jon.p.woodward@gmail.com>
2022-03-05 12:08:10 -0500

    turn manager, level build improvements
    character movement basics...?

--- Scenes/Character3D.gd
+++ Scenes/Character3D.gd
@@ -3,27 +3,47 @@ extends Spatial
 export var player: bool 
 var actions: Array = [
 	## [name, enabled, duration]
-	["crouch", true, 10],
-	["throw", true, 20],
+	["walk", true, 10],
+	["walk", true, 20],
 	["walk", true, 30],
-	["walk2", true, 40],
 	]
+var walk_speed: float = 50
 var current_action
+var parent
+var tween
+var tween_dur
 
 signal action_taken
 
 func _ready():
-	pass # Replace with function body.
+	parent = get_parent()
+	tween = $Tween
 
-
-func _process(delta):
+func _physics_process(delta):
 	pass
 
 func take_turn():
 	if player: 
 		yield()
 	else: 
+		##tween.resume()
 		yield(get_tree().create_timer(3.0), "timeout")
 		var act_index = randi() % actions.size()
 		current_action = actions[act_index]
-		emit_signal("action_taken", current_action)
+		if current_action[0] == "walk":
+			var destination = Vector2()
+			destination.x = randi() % int(parent.board_size.x)
+			destination.y = randi() % int(parent.board_size.y)
+			var dest3D = Vector3(destination.x, 0, destination.y)
+			var dist_to_dest = translation.distance_to(dest3D)
+			current_action[2] = dist_to_dest * walk_speed
+			tween_dur = dist_to_dest * walk_speed / 60
+			tween.interpolate_property(self, "translation", translation, dest3D, tween_dur)
+			tween.start()
+		emit_signal("action_taken", self, current_action)
+
+func on_red_light():
+	tween.stop_all()
+
+func on_green_light():
+	tween.resume_all()
--- Scenes/Level3D.gd
+++ Scenes/Level3D.gd
@@ -3,29 +3,79 @@ extends Spatial
 export var board_size: Vector2 = Vector2(5, 5)
 export var tile_size: float = 1
 var tile = preload("res://Scenes/FloorTile3D.tscn")
-var offset: Vector2
+
+var characters: Array = []
+var turn_tracker: Dictionary = {}
 
 var cam_rig_rot_target: int
 var cam_rig
 
+var turn_marker
+var current_moment: int = 0
+var advancing: bool = true
+
+signal red_light
+signal green_light
+
 func _ready():
+	randomize()
 	cam_rig = $CameraRig
+	turn_marker = $TurnMarker
 	cam_rig_rot_target = cam_rig.rotation_degrees.y
 	build()
+	center_camera()
+	for character in get_tree().get_nodes_in_group("character"):
+		register_character(character)
+		
 
 func build():
+	var tot = board_size.x * board_size.y
+	var rando = randi() % int(tot) + 1
+	var rand_index = 0
 	for x in board_size.x:
 		for y in board_size.y: 
+			rand_index += 1
+#			if rand_index == rando: ## randomly don't place this tile
+#				continue
 			var new_tile = tile.instance()
-			new_tile.translation.x = (x * tile_size) - (board_size.x / 2) + (tile_size / 2)
-			new_tile.translation.z = (y * tile_size) - (board_size.y / 2) + (tile_size / 2)
+			new_tile.translation.x = (x * tile_size)
+			new_tile.translation.z = (y * tile_size)
 			if (x + y) % 2 == 0:
 				new_tile.set_material_override(null)
 			add_child(new_tile)
 
-func _process(delta):
-	## rotate camera rig
+func center_camera():
+	cam_rig.translation.x = (board_size.x * tile_size / 2) - (tile_size / 2)
+	cam_rig.translation.z = (board_size.y * tile_size / 2) - (tile_size / 2)
+
+func register_character(_char):
+	turn_tracker[_char] = 0
+	_char.connect("action_taken", self, "update_turn")
+	self.connect("red_light", _char, "on_red_light")
+	self.connect("green_light", _char, "on_green_light")
+
+func _physics_process(delta):
+	advance_time()
+	resolve_turns()
 	rotate_cam_rig()
+
+func advance_time():
+	if advancing:
+		current_moment += 1
+
+func resolve_turns():
+	if advancing:
+		for turn in turn_tracker:
+			if current_moment >= turn_tracker[turn]:
+				advancing = false
+				emit_signal("red_light")
+				turn_marker.show()
+				turn_marker.translation.x = turn.translation.x
+				turn_marker.translation.z = turn.translation.z
+				yield(turn.take_turn(), "completed")
+				advancing = true
+				emit_signal("green_light")
+				turn_marker.hide()
 
 func rotate_cam_rig():
 	if Input.is_action_just_pressed("ui_left"):
@@ -34,4 +84,8 @@ func rotate_cam_rig():
 		cam_rig_rot_target = (cam_rig_rot_target - 90) % 360
 	if cam_rig.rotation_degrees.y != cam_rig_rot_target:
 		var new_rad = deg2rad(cam_rig_rot_target)
-		cam_rig.rotation.y = lerp_angle(cam_rig.rotation.y, new_rad, 0.05)	
+		cam_rig.rotation.y = lerp_angle(cam_rig.rotation.y, new_rad, 0.05)
+
+func update_turn(node, action):
+	print(node.name + " | " + str(turn_tracker[node] + action[2]))
+	turn_tracker[node] += action[2]
