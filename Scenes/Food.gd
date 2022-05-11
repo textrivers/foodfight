9b228e38c7907400f30c5eb05283f3e12b3d13ab
textrivers <jon.p.woodward@gmail.com>
2022-05-11 14:09:21 -0400

    blueberry and sprite splat improvements

--- Scenes/Food.gd
+++ Scenes/Food.gd
@@ -29,7 +29,6 @@ func _physics_process(delta):
 		var coll = move_and_collide(velocity * delta, false, true, false)
 		velocity.y -= gravity * delta
 		if coll: 
-			print(coll.position.y)
 			for splat_col in splat_colors:
 				spawn_splatter_particles(coll.position, splat_col)
 			if coll.collider.is_in_group("character"):
--- Scenes/SplatParticles.gd
+++ Scenes/SplatParticles.gd
@@ -5,26 +5,26 @@ var porous: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	if randi() % 2 == 0:
+	if randi() % 3 == 0:
 		porous = true
 	get_parent().connect("red_light", self, "_on_red_light")
 	get_parent().connect("green_light", self, "_on_green_light")
 	material_override = material_override.duplicate()
 	material_override.albedo_texture = $Viewport.get_texture()
 	if porous:
-		$AnimationPlayer.play("splort")
+		$Viewport/PartSprite/Light2D.show()
+	$AnimationPlayer.play("splort")
+	$Viewport/PartSprite/Sprite2.offset = Vector2(randf() * 84, randf() * 84)
 
 func _on_red_light():
 	speed_scale = 0
 	$Timer.set_paused(true) 
-	if porous:
-		$AnimationPlayer.stop(false)
+	$AnimationPlayer.stop(false)
 
 func _on_green_light():
 	speed_scale = 1
 	$Timer.set_paused(false) 
-	if porous:
-		$AnimationPlayer.play()
+	$AnimationPlayer.play()
 
 func _on_Timer_timeout():
 	call_deferred("queue_free")
