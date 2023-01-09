f2f3bec678d2bdbdbb518e65afe859cc1e05c000
textrivers <jon.p.woodward@gmail.com>
2023-01-09 17:09:47 -0500

    more groundwork for text gallery

---
+++ Scenes/TextGallery.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+## TODO create a Texture Button script; use this to send a signal up to this script
+## signal handler initiates tween for the sprite scale and location to center of screen, 
+## also the modulate to darken all else
+## and stores the scale/location to tween back to on any input
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
