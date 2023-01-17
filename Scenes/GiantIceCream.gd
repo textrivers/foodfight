38daf004991c7ff64c8cbc912206ff8b486bc4d2
textrivers <jon.p.woodward@gmail.com>
2023-01-17 08:42:55 -0500

    small ice cream changes

--- Scenes/GiantIceCream.gd
+++ Scenes/GiantIceCream.gd
@@ -10,8 +10,10 @@ func _process(delta):
 	var dist_to_player = self.global_translation.distance_to(Global.player_node.global_translation)
 	if dist_to_player <= 10: 
 		$AnimatedSprite3D.frame = int(10 - dist_to_player)
-		scale = Vector3.ONE + (scale * ((10 - dist_to_player) / 10))
+		$AnimatedSprite3D.scale = Vector3.ONE + (scale * ((10 - dist_to_player) / 15))
 
 func _on_Area_body_entered(body):
+	Global.hilarity = 0
+	Global.visible_splat_count = 0
 	if body.player:
 		SceneManager.goto_scene(get_parent(), nextscene)
