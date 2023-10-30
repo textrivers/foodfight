e84be0a052ff73da60e32c808bf99cd7da91b41f
textrivers <jon.p.woodward@gmail.com>
2023-10-30 13:38:50 -0400

    navigation instruction level 1

---
+++ Scenes/LookAroundInstructions.gd
@@ -0,0 +1,16 @@
+extends Label
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _input(event):
+	if event is InputEventMouseMotion && Input.is_action_pressed("right_click"):
+		self.queue_free()
